26ed09ff17be309421b5d75e84cb3e4730f1c822
Earl Brown <github@theGleep.com>
2021-12-27 19:51:58 -0700

    Initial population from clone

---
+++ w/api251f.php
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.mediawiki.org/w/api.php?action=rsd by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Dec 2021 00:32:55 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=api251f.php?action=rsd"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="api251f.php?action=rsd"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.mediawiki.org/w/api.php?action=rsd by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Dec 2021 00:32:55 GMT -->
+</HTML>
